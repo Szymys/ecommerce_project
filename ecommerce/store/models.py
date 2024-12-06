f61b487ef153931ceaa207098e4f5d173001a691
Szymon <103196453+Szymys@users.noreply.github.com>
2024-12-06 15:05:41 +0100

    Add files via upload

---
+++ ecommerce/store/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+from django.urls import reverse
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=250, db_index=True)
+
+    slug = models.SlugField(max_length=250, unique=True)
+
+
+# ZMIANA NAZWY BO INNACZEJ WYSWIETLA CATEGORYS
+    class Meta:
+
+        verbose_name_plural = 'categories'
+
+# WYSWIETLA NAZWE A NIE NP. CATEGORY (1)
+    def __str__(self):
+
+        return self.name
+    
+    def get_absolute_url(self):
+
+        return reverse("list-category", args=[self.slug])
+    
+
+
+
+
+class Product(models.Model):
+
+    # mozliwosc dodawania kategorii w produktach
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
+
+    title = models.CharField(max_length=250)
+
+    brand = models.CharField(max_length=250, default = 'un-branded')
+
+    description = models.TextField(blank=True)
+
+    slug = models.SlugField(max_length=250)
+
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+# PRZENOSI ZDJECIA DO FOLDERU MEDIA 
+    image = models.ImageField(upload_to='images/')
+
+
+    class Meta:
+
+        verbose_name_plural = 'products'
+
+
+# ZEBY NIE BYLO PRODUKT(2) itp
+    def __str__(self):
+
+        return self.title
+
+
+    def get_absolute_url(self):
+
+        return reverse('product-info', args=[self.slug])
+
+
+
