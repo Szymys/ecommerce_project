1d1148b41205381c9ea87da4c046a5ee3e1b943e
Szymon <103196453+Szymys@users.noreply.github.com>
2024-12-12 20:30:36 +0100

    Add files via upload

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+# AUTOMATYCZNIE DODAJE SLUG JAK WPROWADZAM NAME
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+
+
