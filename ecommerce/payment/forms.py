636a31b85345a9ebfd3c6e17ed356dee7527b69e
Szymon <103196453+Szymys@users.noreply.github.com>
2025-01-19 01:46:56 +0100

    Add files via upload

--- ecommerce/payment/forms.py
+++ ecommerce/payment/forms.py
@@ -1,15 +1,15 @@
 
 from django import forms
 
-from . models import ShippingAddress
+from .models import ShippingAddress
 
 class ShippingForm(forms.ModelForm):
 
-    class Mera:
+    class Meta:
 
         model = ShippingAddress
 
-        fields = ['full_name', 'email', 'adress1', 'address2', 'city','state' ,'zipcode', ]
+        fields = ['full_name', 'email', 'address1', 'address2', 'city','state' ,'zipcode', ]
 
         exclude = ['user',]
 
