566b0856bb7b1d94b5e82270d6b089e7b7a68bfd
szymys <szymon.mysliwiec@gmail.com>
2025-04-27 19:17:53 +0200

    email_verifi

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -150,3 +150,13 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  #backend do wysylania maili w konsoli
+
+
+
+
+
+
+
